b5524df13b86569f8b9d48520579ed574360e49c
sebsan89 <seb.sannac@gmail.com>
2021-11-22 14:23:38 +0100

    Fix the BUG when you buy more space than available points with a condition

--- server.py
+++ server.py
@@ -50,8 +50,12 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+    if placesRequired < int(club['points']) and placesRequired < int(competition['numberOfPlaces']):
+        club['points'] = int(club['points']) - placesRequired
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+        flash('Great-booking complete!')
+    else:
+        flash("Reservation is impossible!")
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
